ee641e75743d64f584dd714cae3d840553e5f640
Rian <rian.azevedo@qintess.com>
2024-04-05 00:18:45 -0300

    change: -Altura espaçamento e paddings de Prioridades e shorttasks

--- src/components/ShortTask/styles.js
+++ src/components/ShortTask/styles.js
@@ -3,18 +3,18 @@ import styled from 'styled-components';
 export const Container = styled.button`
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   color: ${({ theme }) => theme.COLORS.GRAY_100};
-  height: 44px;
+  height: 2.75rem;
   border: 0;
-  padding: 0 16px;
-  border-radius: 7px;
-  font-size: 14px;
+  padding: 0 1rem;
+  border-radius: 0.4rem;
+  font-size: 0.87rem;
   
   display: flex;
   align-items: center;
   gap: 7px;
 
   > svg {
-    font-size: 18px;
+    font-size: 1.12rem;
     color: ${({ theme }) => theme.COLORS.RED_200};
   }
 `;
